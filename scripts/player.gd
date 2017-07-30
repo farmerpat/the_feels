5f54a7068c1dd3a168f24ccd21b87c987498797e
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2017-07-30 17:43:28 -0400

    init the feels including a stub of animation (grow)

---
+++ scripts/player.gd
@@ -0,0 +1,41 @@
+extends RigidBody2D
+
+const MOVE_SPEED = 50
+const ACCEL = 500
+
+var screen_size = Vector2()
+var vel = Vector2()
+var acc = Vector2()
+
+func _ready():
+	# for now, center the player to start
+	screen_size = get_viewport_rect().size
+	#var pos = screen_size / 2
+	#set_pos(pos)
+	#set_fixed_process(true)
+	#set_process_input(true)
+
+#func _input(event):
+	#pass
+	#vel.x = event.is_action_pressed("ui_right") - event.is_action_pressed("ui_left")
+	#vel.y = event.is_action_pressed("ui_down") - event.is_action_pressed("ui_up")
+	#apply_impulse(Vector2(), vel)
+	##vel.x *= MOVE_SPEED
+	##vel.y *= MOVE_SPEED
+
+func _integrate_forces(state):
+	vel.x = Input.is_action_pressed("ui_right") - Input.is_action_pressed("ui_left")
+	vel.y = Input.is_action_pressed("ui_down") - Input.is_action_pressed("ui_up")
+	#print(vel)
+	set_applied_force(Vector2(1,1))
+
+#func _fixed_process(delta):
+#	print(vel)
+	##acc.x = Input.is_action_pressed("ui_right") - Input.is_action_pressed("ui_left")
+	##acc.y = Input.is_action_pressed("ui_down") - Input.is_action_pressed("ui_up")
+	##vel += acc * delta
+	#vel.x = Input.is_action_pressed("ui_right") - Input.is_action_pressed("ui_left")
+	#vel.y = Input.is_action_pressed("ui_down") - Input.is_action_pressed("ui_up")
+	##add_force(Vector2(0,0), vel)
+	#apply_impulse(Vector2(0,0), vel)
+	##move(vel * delta)
