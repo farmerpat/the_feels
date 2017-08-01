694e1c55e7c37f350eeb91c610011f5d7b9dbc5b
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2017-07-31 22:24:07 -0400

    init enemy and proof of concept for player feel scale anim

--- scenes/player.gd
+++ scenes/player.gd
@@ -25,8 +25,11 @@ func _input(event):
 	if event.is_action_pressed("ui_select"):
 		# we might have to create the animation node on the fly to know what size we're
 		# starting w/ and going to...
-		pos_feels_animator.play("grower")
-		var old_size = pos_feels.get_scale()
+		#pos_feels_animator.play("grower")
+		grow_pos()
+
+
+		#var old_size = pos_feels.get_scale()
 		#pos_feels.set_scale(old_size * 1.01)
 	#vel.x = event.is_action_pressed("ui_right") - event.is_action_pressed("ui_left")
 	#vel.y = event.is_action_pressed("ui_down") - event.is_action_pressed("ui_up")
@@ -39,6 +42,46 @@ func _integrate_forces(state):
 	vel.y = Input.is_action_pressed("ui_down") - Input.is_action_pressed("ui_up")
 	#print(vel)
 	set_applied_force(vel * MOVE_SPEED)
+
+func grow_pos_punk_out():
+	pass
+
+func grow_pos():
+	print("in grow pos")
+	
+	# I want to be able to do it this way, but
+	# it will probably be faster to programatically edit the values
+	# of the four keyframes
+	# ala https://www.youtube.com/watch?v=5m9eB6bq3t0
+
+	var anim = Animation.new()
+	anim.set_length(3.1)
+	var track = anim.add_track(Animation.TYPE_VALUE)
+	anim.track_set_path(track, ".:transform/scale")
+
+	var current_scale = pos_feels.get_scale()
+	print (current_scale)
+	anim.track_insert_key(track, 0.0, current_scale)
+	anim.track_insert_key(track, 1.0, Vector2(current_scale.x + .03, current_scale.y + .03))
+	anim.track_insert_key(track, 2.0, Vector2(current_scale.x + .06, current_scale.y + .06))
+	anim.track_insert_key(track, 3.0, Vector2(current_scale.x + .1, current_scale.y + .1))
+
+	print (anim.get_track_count())
+	print(anim.get_step())
+	print(anim.track_get_type(0))
+	print(anim.track_get_path(0))
+	print(anim.track_get_key_value(0, 0))
+	print(anim.track_get_key_value(0,1))
+
+	print(pos_feels_animator.get_animation_list())
+	pos_feels_animator.add_animation("dis_grower", anim)
+	pos_feels_animator.play("dis_grower")
+	print(pos_feels_animator)
+	
+	print ('np:')
+	var p = get_node("pos_feels:transform/scale")
+	print(p)
+	#pos_feels_animator.remove_animation("dis_grower")
 
 #func _fixed_process(delta):
 #	print(vel)
