1d0b1bf9fb1b428514eb0e159e29a7a53a02c9f7
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2017-07-31 22:43:06 -0400

    init bullet

---
+++ scripts/bullet.gd
@@ -0,0 +1,22 @@
+extends RigidBody2D
+
+var fire_force = Vector2()
+var fire_speed = 100
+var need_to_fire = false
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	pass
+	#fire(Vector2(-1, 0))
+
+func fire(dir_vec):
+	fire_force = dir_vec
+	# probably firespeed should be an optional argument
+	fire_force *= fire_speed
+	need_to_fire = true
+
+func _integrate_forces(state):
+	if need_to_fire:
+		set_applied_force(fire_force)
