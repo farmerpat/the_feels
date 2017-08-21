d627276bf63af1bb0b46887a3ce546df68436d11
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2017-08-21 00:54:23 -0400

    change bullet color with shooter color change

--- Assets/Scripts/MyPlayerController.cs
+++ Assets/Scripts/MyPlayerController.cs
@@ -9,8 +9,7 @@ public class MyPlayerController : MonoBehaviour {
 	private Rigidbody2D body;
 	private GameObject svgSorter;
 	private GameObject shooter = null;
-	private GameObject posBullet;
-	private GameObject negBullet;
+	private GameObject bulletToFire;
 
 	private float positiveInputTolerance;
 	private float negativeInputTolerance;
@@ -26,6 +25,7 @@ public class MyPlayerController : MonoBehaviour {
 	private bool toggleShooterFeel = false;
 	private bool changingShooterColor = false;
 	private Color shooterColorToBecome;
+	private string currentShooterFeel = "pos";
 
 	void Start () {
 		positiveInputTolerance = deadZoneSize;
@@ -87,8 +87,13 @@ public class MyPlayerController : MonoBehaviour {
 		// y = 2.19 * sin(theta)
 		// x = 2.19 * cos(theta)
 
-		// if we're in pos shoot mode, which doesn't exist yet...
-		posBullet = (GameObject)Instantiate(Resources.Load("PosBullet"));
+		if (currentShooterFeel == "pos") {
+			bulletToFire = (GameObject)Instantiate(Resources.Load("PosBullet"));
+
+		} else {
+			bulletToFire = (GameObject)Instantiate(Resources.Load("NegBullet"));
+
+		}
 
 		Vector3 playerOrigin = transform.position;
 		float spawnPointDistFromPlayerOrigin = 2.19f;
@@ -126,13 +131,13 @@ public class MyPlayerController : MonoBehaviour {
 		bulletXPos += playerOrigin.x;
 		bulletYPos += playerOrigin.y;
 
-		posBullet.transform.position = new Vector3(bulletXPos, bulletYPos);
+		bulletToFire.transform.position = new Vector3(bulletXPos, bulletYPos);
 
 		Quaternion rot = Quaternion.AngleAxis(angle,Vector3.forward);
 
 		// might be able to skip the calculation above since transform.rotation might already be the Quaternion we want
 		Vector3 bulletMovementUnit = rot * Vector3.right;
-		posBullet.GetComponent<Rigidbody2D>().AddForce(bulletMovementUnit * 100.0f);
+		bulletToFire.GetComponent<Rigidbody2D>().AddForce(bulletMovementUnit * 300.0f);
 
 
 		// create a bullet script with a fire method, add it to PosBullet and NegBullet
@@ -144,7 +149,6 @@ public class MyPlayerController : MonoBehaviour {
 		// https://blogs.msdn.microsoft.com/nathalievangelist/2014/12/16/joystick-input-in-unity-using-xbox360-controller/
 		// http://wiki.unity3d.com/index.php?title=Xbox360Controller
 		// it looks like linux/win have different joystick axes than osx. cool.
-		//body.MoveRotation(2.0f);
 		movementUnit.x = 0;
 		movementUnit.y = 0;
 
@@ -163,9 +167,6 @@ public class MyPlayerController : MonoBehaviour {
 			movementUnit.x = -1;
 
 		}
-
-		//this.crudeShooter ();
-		//body.MoveRotation(datAngle);
 
 		if (this.haveRightStickInput ()) {
 			float ex = Input.GetAxis ("HorizontalRight");
@@ -186,7 +187,6 @@ public class MyPlayerController : MonoBehaviour {
 			// should this be in FixedUpdate or what?
 			// should the speed, here, 0.1f, me multiplied by Time.time or Time.deltaTime?
 			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler(new Vector3(0.0f, 0.0f, angle)), 0.1f);
-
 
 			/*
 			 *  consider using SVG colliders
@@ -266,8 +266,6 @@ public class MyPlayerController : MonoBehaviour {
 			}
 		}
 
-		// probably must use white for the stroke and fill of orbital
-		// then apply color manually
 		// neg: #C66174FF
 		// 		198 97 116
 		// pos: #4C71B9FF
@@ -279,28 +277,14 @@ public class MyPlayerController : MonoBehaviour {
 			Color negFeelsColor = new Color(198.0f/255.0f, 97.0f/255.0f, 116.0f/255.0f, 1);
 			Color posFeelsColor = new Color(76.0f/255.0f, 113.0f/255.0f, 185.0f/255.0f, 1);
 
-			if (shooter) {
-				SVGImporter.SVGRenderer shooterSVGRenderer = shooter.GetComponent<SVGImporter.SVGRenderer> ();
+			if (currentShooterFeel == "pos") {
+				currentShooterFeel = "neg";
+				shooterColorToBecome = negFeelsColor;
 
-				if (shooterSVGRenderer) {
-					// a flag instead of all this math is a better idea
-					Color currentColor = shooterSVGRenderer.color;
-					int r = Mathf.RoundToInt(255.0f * currentColor.r);
-					int g = Mathf.RoundToInt(255.0f * currentColor.g);
-					int b = Mathf.RoundToInt(255.0f * currentColor.b);
-					int a = Mathf.RoundToInt(255.0f * currentColor.a);
+			} else {
+				currentShooterFeel = "pos";
+				shooterColorToBecome = posFeelsColor;
 
-					// we should have a variable that holds the name of the current color state,
-					// because if player tries to change color while lerping, it won't do
-					// its going to be white until we fix it...
-					if (r == 76 && g == 113 && b == 185) {
-						shooterColorToBecome = negFeelsColor;
-
-					} else {
-						shooterColorToBecome = posFeelsColor;
-
-					}
-				}
 			}
 		}
 
@@ -309,6 +293,8 @@ public class MyPlayerController : MonoBehaviour {
 			// we can't test if shooterColorToBecome is null or set it to null because
 			// unity complains.  figure out a way around this, so we aren't assuming
 			// blindly that shooterColorToBecome is available.
+			//
+			// consider gradually changing color of bullets too
 
 			// see about not duplicating this (it appears above)
 			SVGImporter.SVGRenderer shooterSVGRenderer = shooter.GetComponent<SVGImporter.SVGRenderer> ();
